d0b6016de0f7eb436cd59d72434d65e844a109f9
teokri303 <74730383+teokri303@users.noreply.github.com>
2024-02-21 22:49:05 +0200

    accordion ready
    accordion ready remain to implement maybe some motion and center the divs. also make it on desktop also

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  Box,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import LanguageSwitcher from "./Language_switcher";
 
 const HorizontalAccordion = () => {
@@ -22,7 +15,7 @@ const HorizontalAccordion = () => {
   };
 
   useEffect(() => {
-    console.log(openIndex);
+    //console.log(openIndex);
   }, [openIndex]);
 
   const content = [
@@ -33,24 +26,34 @@ const HorizontalAccordion = () => {
     <LanguageSwitcher />,
   ];
   return (
-    <div>
-      <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+    <div style={{ marginTop: "5px", marginBottom: "10px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "row",
+          gap: "10px",
+        }}
+      >
         {Array.from({ length: 5 }).map((_, index) => (
           <div
             key={index}
             style={{
-              width: "100px",
-              height: "100px",
-              backgroundColor: openIndex === index ? "skyblue" : "lightgray",
+              width: "55px",
+              height: "45px",
+              backgroundColor: openIndex === index ? "teal" : "transparent",
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
-              cursor: "pointer",
               border: "1px solid black",
+              borderColor: "teal",
+              borderRadius: "15%",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.5)",
             }}
             onClick={() => handleAccordionClick(index)}
           >
-            Box {index + 1}
+            {index === 4 ? "en/gr" : `Tip ${index + 1}`}
           </div>
         ))}
       </div>
@@ -58,8 +61,7 @@ const HorizontalAccordion = () => {
         {openIndex !== -1 && (
           <div
             style={{
-              marginTop: "20px",
-              border: "1px solid grey",
+              marginTop: "2px",
               padding: "10px",
             }}
           >
@@ -67,165 +69,6 @@ const HorizontalAccordion = () => {
           </div>
         )}
       </div>
-      <Accordion allowToggle marginBottom="3px">
-        <Box
-          display="flex"
-          justifyContent="center"
-          flexDirection="row"
-          gap={1}
-          overflow="auto" // Προσθήκη ιδιότητας overflow
-        >
-          <AccordionItem>
-            <Box
-              display="flex"
-              alignItems="center"
-              border="1px"
-              p={3}
-              borderRadius="md"
-              borderColor="teal"
-              boxShadow="0 4px 6px rgba(0, 0, 0, 0.4)"
-            >
-              <AccordionButton
-                padding={0}
-                _expanded={{ textColor: "teal" }}
-                onClick={() => handleAccordionClick(1)}
-              >
-                <Text fontSize="sm" fontWeight="bold">
-                  Tip 1
-                </Text>
-              </AccordionButton>
-            </Box>
-            <AccordionPanel>
-              <Text fontSize="xs" textAlign="left">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <Box
-              display="flex"
-              alignItems="center"
-              border="1px"
-              p={3}
-              borderRadius="md"
-              borderColor="teal"
-              boxShadow="0 4px 6px rgba(0, 0, 0, 0.4)"
-            >
-              <AccordionButton
-                padding={0}
-                _expanded={{ textColor: "teal" }}
-                onClick={() => handleAccordionClick(2)}
-              >
-                <Text fontSize="sm" fontWeight="bold">
-                  Tip 2
-                </Text>
-              </AccordionButton>
-            </Box>
-            <AccordionPanel>
-              <Text fontSize="xs" textAlign="left">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <Box
-              display="flex"
-              alignItems="center"
-              border="1px"
-              p={3}
-              borderRadius="md"
-              borderColor="teal"
-              boxShadow="0 4px 6px rgba(0, 0, 0, 0.4)"
-            >
-              <AccordionButton
-                padding={0}
-                _expanded={{ textColor: "teal" }}
-                onClick={() => handleAccordionClick(3)}
-              >
-                <Text fontSize="sm" fontWeight="bold">
-                  Tip 3
-                </Text>
-              </AccordionButton>
-            </Box>
-            <AccordionPanel>
-              <Text fontSize="xs" textAlign="left">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <Box
-              display="flex"
-              alignItems="center"
-              border="1px"
-              p={3}
-              borderRadius="md"
-              borderColor="teal"
-              boxShadow="0 4px 6px rgba(0, 0, 0, 0.4)"
-            >
-              <AccordionButton
-                padding={0}
-                _expanded={{ minWidth: "100px", textColor: "teal" }}
-                onClick={() => handleAccordionClick(4)}
-              >
-                <Text fontSize="sm" fontWeight="bold">
-                  Tip 4
-                </Text>
-              </AccordionButton>
-            </Box>
-            <AccordionPanel>
-              <Text maxWidth="200px" fontSize="xs" textAlign="left">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim
-                ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                ut aliquip ex ea commodo consequat. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <Box
-              display="flex"
-              alignItems="center"
-              border="1px"
-              p={3}
-              borderRadius="md"
-              borderColor="teal"
-              boxShadow="0 4px 6px rgba(0, 0, 0, 0.4)"
-            >
-              <AccordionButton
-                padding={0}
-                _expanded={{ textColor: "teal" }}
-                onClick={() => handleAccordionClick(5)}
-              >
-                <Text fontSize="sm" fontWeight="bold">
-                  en/gr
-                </Text>
-              </AccordionButton>
-            </Box>
-            <AccordionPanel>
-              <LanguageSwitcher />
-            </AccordionPanel>
-          </AccordionItem>
-        </Box>
-      </Accordion>
     </div>
   );
 };
